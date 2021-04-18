3de85b6982a0105bae4eae3aaca134840f2308aa
andrea.lambruschini <andrea.lambruschini@telecomitalia.it>
2021-04-18 10:01:54 +0200

    Aggiunto bozza api motors

--- components/drivers/motors/include/motors.h
+++ components/drivers/motors/include/motors.h
@@ -14,14 +14,47 @@
 
 #define MOTORS_MAX_NUM 8
 
+// TODO: Define all positions from quad to opto
+typedef enum {
+	FRONT,
+	REAR,
+	LEFT,
+	RIGHT,
+	FRONT_LEFT,
+	FRONT_RIGHT,
+	REAR_LEFT,
+	REAR_RIGHT
+} motor_position_t;
+
+typedef enum {
+	ON_DISARMED,
+	ON_ARMED,
+	OFF
+} motors_status_t;
+
 typedef struct {
 	uint8_t num;
 	uint8_t pin;
+	motor_position_t position;
 	mcpwm_dev_t* mcpwm;
+	float duty_cycle;
 } motor_t;
 
 typedef struct {
 	motor_t motor[MOTORS_MAX_NUM];
+	uint16_t frequency;
+	uint8_t motors_num;
+	uint8_t switch_on_off_pin;
+	motors_status_t status;
 } motors_t;
+
+typedef motors_t* motors_handle_t;
+
+esp_err_t motors_init(motors_handle_t motors_handle);
+esp_err_t motors_arm(motors_handle_t motors_handle);
+esp_err_t motors_disarm(motors_handle_t motors_handle);
+esp_err_t motors_switchoff(motors_handle_t motors_handle);
+esp_err_t motors_switchon(motors_handle_t motors_handle, bool program_esc);
+
 
 #endif /* COMPONENTS_MOTORS_INCLUDE_MOTORS_H_ */
--- components/drivers/motors/src/motors.c
+++ components/drivers/motors/src/motors.c
@@ -1,2 +1,12 @@
 // Da implementare
 #include <motors.h>
+
+static mcpwm_dev_t *MCPWM[2] = {&MCPWM0, &MCPWM1};
+
+/************************************************************************
+ ****************** A P I  I M P L E M E N T A T I O N ******************
+ ************************************************************************/
+
+esp_err_t motors_init(motors_handle_t motors_handle) {
+
+}
